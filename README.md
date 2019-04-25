20566c7f0484d8a326465a6a0bd18bbd89369f19
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-04-25 15:05:20 +0100

    Added a new project created from angular cli Adde date picker added time picker

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # AngularFormsNgxBootstrap
 
-This is an old angular project and does not uses angular cli
-to start it user `npm start` 
-the server port is defined on the package.json file 
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.0.
+
+This is a project to show ngxBootstrap libraries
+#Dependencies that need installing 
+
+  npm install ngx-bootstrap bootstrap --save
+  ng add ngx-bootstrap  --component datepicker 
+  ng add ngx-bootstrap  --component timepicker
+  ng add ngx-bootstrap  --component buttons
 
 
-This is a sample project to show how to use ngxBootstrap to make nice user einterfaces for 
-datepicker
-toggles
-time picker...
+## Development server
 
-#Dependencies that need isntalling
-  npm install ngx-bootstrap --save
-  npm install --save tslib
-  npm install rxjs@6 rxjs-compat@6 --save
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,29 @@
-import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router';
-import {rootRouterConfig} from './app.routes';
-import {AppComponent} from './app.component';
-import {GithubService} from './github/shared/github.service';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
 
-import {AboutComponent} from './about/about.component';
-import {HomeComponent} from './home/home.component';
-import {RepoBrowserComponent} from './github/repo-browser/repo-browser.component';
-import {RepoListComponent} from './github/repo-list/repo-list.component';
-import {RepoDetailComponent} from './github/repo-detail/repo-detail.component';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
-import {ContactComponent} from './contact/contact.component';
-import {DatePickerComponent, DatepickerModule} from "ngx-bootstrap";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AlertModule, BsDatepickerModule} from 'ngx-bootstrap';
+import {FormsModule} from '@angular/forms';
+import {TimepickerModule} from 'ngx-bootstrap/timepicker';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    RepoBrowserComponent,
-    RepoListComponent,
-    RepoDetailComponent,
-    HomeComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
+    TimepickerModule.forRoot(),
+    BsDatepickerModule.forRoot(),
     BrowserModule,
-    DatepickerModule,
+    AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
-    RouterModule.forRoot(rootRouterConfig, {useHash: true})
+    TimepickerModule.forRoot(),
+    ButtonsModule.forRoot()
   ],
-  providers: [
-    GithubService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
 }
